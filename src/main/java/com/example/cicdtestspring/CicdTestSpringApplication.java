09d4831452094f206d59d9820500cb07dbfc20bf
YunJae00 <0109548@naver.com>
2024-08-22 19:45:31 +0900

    Initial commit

---
+++ src/main/java/com/example/cicdtestspring/CicdTestSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdtestspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdTestSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdTestSpringApplication.class, args);
+    }
+
+}
