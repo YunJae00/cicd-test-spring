00d77dc877328789ac32e53de880fb48cc116995
YunJae00 <0109548@naver.com>
2024-08-22 20:06:31 +0900

    ini

---
+++ src/main/java/com/example/cicdtestspring/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.example.cicdtestspring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "get test mapping";
+    }
+
+    @GetMapping("/test2")
+    public String test2() {
+        return "get test2 mapping";
+    }
+}
